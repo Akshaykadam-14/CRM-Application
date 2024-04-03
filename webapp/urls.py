cab627b6c96e2ce801f675aed9634dd893f66e39
Akshaykadam-14 <akshaykadam7608@gmail.com>
2024-04-03 14:36:55 +0530

    Second commit

--- webapp/urls.py
+++ webapp/urls.py
@@ -9,9 +9,17 @@ urlpatterns = [
     path('my-login', views.my_login, name="my-login"),
 
     path('user-logout', views.user_logout, name="user-logout"),
-
+    # CRUD
     path('dashboard', views.dashboard, name="dashboard"),
+    
+    path('create-record', views.create_record, name="create-record"),
+    
+    path('update-record/<int:pk>', views.update_record, name='update-record'),
 
+    path('record/<int:pk>', views.singular_record, name="record"),
+
+    path('delete-record/<int:pk>', views.delete_record, name="delete-record"),
+    
 
 
 ]
